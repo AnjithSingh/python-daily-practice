b5a598d9b79207111d14ad35d8429fa30dce491e
AnjithSingh <anjithsinght@gmail.com>
2026-01-16 20:14:20 +0530

    Updated README and fixed bug

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask , request, redirect , url_for ,session , Response
+
+app = Flask(__name__)
+app.security_key = 'Hehahah'
+
+#Home page
+
+@app.route("/" , methods = ["GET" , "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == "admin" and password == "123":
+            session["user"] = username
+            return redirect(url_for("welcome"))
+        else:
+            return Response("In-valid credentials. Try again!" , mimetype = "text/plain")
+    return '''
+             <h2>login page </h2>
+             <form method = "POST">
+             username: <input type = 'text' name = "username">
+             <br>
+             password: <input type = 'text' name = "password">
+             <br>
+             <input type = "submit" value = "login">
+             </form>
+
+
+'''
+#welcome page
+@app.route("/welcome")
+def welcome():
+    if "user" in session:
+        return f'''
+        <h2> Welcome , {session["user"]}! <h2>
+        <a href = {url_for("logout")}> Logout </a>
+'''
+    return redirect(url_for("logint"))
+
+# logout route
+@app.route("/logout")
+def logout():
+    session.pop("user",None)
+    return redirect(url_for("login"))
