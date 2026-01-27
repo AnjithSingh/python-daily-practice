36e9eb618665169cb54592209fc9dbafc2badda7
AnjithSingh <anjithsingh1t@gmail.com>
2026-01-27 13:52:50 +0530

    debugging the code

--- app3.py
+++ app3.py
@@ -12,3 +12,24 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+@app.route('/user/<username>')
+def show_user(username):
+    # Greet the user
+    return f'Hello {username} !'
+
+# Pass the required route to the decorator.
+@app.route("/hello")
+def hello():
+    return "Hello, Welcome to ViralHit"
+  
+@app.route("/")
+def index():
+    return "Homepage of ViralHit"
+
+
