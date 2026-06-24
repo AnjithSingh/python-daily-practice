75bf97852768b6d095a5cbe61c02f5cad8a547c9
AnjithSingh <anjithsingh1t@gmail.com>
2026-06-25 00:26:49 +0530

    methods

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask , request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "hello user! this is our fist flask app"
+
+@app.route("/about")
+def about():
+    return "this is about us page"
+
+@app.route("/contact")
+def contact():
+    return "this is our contact page"
+
+@app.route("/submit" , methods = ["GET" , "POST"])
+def submit():
+    if request.method == "POST":
+        return "You are sendind data"
+    else:
+        return "you are just viewing this form"
